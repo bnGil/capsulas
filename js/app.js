a5974403069e3a643f098cc17ee91836298daca3
Samer taha <alrazy1@gmail.com>
2022-05-11 16:15:04 +0300

    OK

--- js/app.js
+++ js/app.js
@@ -6,9 +6,9 @@ async function getFetchData(url) {
 
 async function getPeople() {
   const shortPeopleURL =
-    "https://capsules-asb6.herokuapp.com/api/teacher/mordi";
+    'https://capsules-asb6.herokuapp.com/api/teacher/mordi';
   const peopleShortData = await getFetchData(shortPeopleURL);
-  const personAPI = "https://capsules-asb6.herokuapp.com/api/user/";
+  const personAPI = 'https://capsules-asb6.herokuapp.com/api/user/';
   const peopleFullData = await Promise.all(
     peopleShortData.map((person) => getFetchData(personAPI + person.id))
   );
