06cd043eef90fc411909f873549fd33f8af0580e
Samer taha <alrazy1@gmail.com>
2022-05-11 16:28:34 +0300

    delete and edit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-const input = document.getElementById("input");
-const body = document.querySelector("body");
-const container = document.querySelector(".container");
+const input = document.getElementById('input');
+const body = document.querySelector('body');
+const container = document.querySelector('.container');
 
 async function getFetchData(url) {
   const res = await fetch(url);
@@ -20,17 +20,17 @@ async function getArrOfPeople() {
 }
 
 const addTitleRow = (title) => {
-  const titleBox = document.createElement("div");
-  titleBox.classList.add("title");
+  const titleBox = document.createElement('div');
+  titleBox.classList.add('title');
   titleBox.textContent = title;
   container.appendChild(titleBox);
 };
 const addRow = (arrOfData) => {
-  const row = document.createElement("div");
-  row.classList.add("row");
+  const row = document.createElement('div');
+  row.classList.add('row');
   arrOfData.forEach((e) => {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
     cell.textContent = e;
     row.appendChild(cell);
   });
@@ -39,14 +39,21 @@ const addRow = (arrOfData) => {
 };
 
 const addButtons = (row) => {
-  const editBtn = document.createElement("button");
+  const editBtn = document.createElement('button');
   row.appendChild(editBtn);
-  editBtn.innerText = "Edit";
-  editBtn.classList.add("btn", "edit-btn");
-  const deleteBtn = document.createElement("button");
+  editBtn.innerText = 'Edit';
+  editBtn.classList.add('btn', 'edit-btn');
+  editBtn.addEventListener('click', (e) => {
+    console.log('edit clicked');
+  });
+
+  const deleteBtn = document.createElement('button');
   row.appendChild(deleteBtn);
-  deleteBtn.innerText = "Delete";
-  deleteBtn.classList.add("btn", "delete-btn");
+  deleteBtn.innerText = 'Delete';
+  deleteBtn.classList.add('btn', 'delete-btn');
+  deleteBtn.addEventListener('click', (e) => {
+    console.log('delete clicked');
+  });
 };
 
 const drawTable = (arrOfData) => {
