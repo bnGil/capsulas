faa5c58794a64844689e1cd7ad5daf4d876054c3
bnGil <gilbn1994@gmail.com>
2022-05-11 13:49:15 +0300

    upload initial files and fetched people

---
+++ js/app.js
@@ -0,0 +1,18 @@
+async function getFetchData(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+}
+
+async function getPeople() {
+  const shortPeopleURL =
+    "https://capsules-asb6.herokuapp.com/api/teacher/mordi";
+  const peopleShortData = await getFetchData(shortPeopleURL);
+  const personAPI = "https://capsules-asb6.herokuapp.com/api/user/";
+  const peopleFullData = await Promise.all(
+    peopleShortData.map((person) => getFetchData(personAPI + person.id))
+  );
+  console.log(peopleFullData);
+}
+
+getPeople();
