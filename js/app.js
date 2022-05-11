bca3852bf41b558b4aef55aa0d09c0daf5bc90bc
bnGil <gilbn1994@gmail.com>
2022-05-11 15:49:11 +0300

    static page without styling

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
+const input = document.getElementById("input");
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
+
 async function getFetchData(url) {
   const res = await fetch(url);
   const data = await res.json();
   return data;
 }
 
-async function getPeople() {
+async function getArrOfPeople() {
   const shortPeopleURL =
     "https://capsules-asb6.herokuapp.com/api/teacher/mordi";
   const peopleShortData = await getFetchData(shortPeopleURL);
@@ -12,7 +16,54 @@ async function getPeople() {
   const peopleFullData = await Promise.all(
     peopleShortData.map((person) => getFetchData(personAPI + person.id))
   );
-  console.log(peopleFullData);
+  return peopleFullData;
 }
 
-getPeople();
+const addTitleRow = (title) => {
+  const titleBox = document.createElement("div");
+  titleBox.classList.add("title");
+  titleBox.textContent = title;
+  container.appendChild(titleBox);
+};
+const addRow = (arrOfData) => {
+  const row = document.createElement("div");
+  row.classList.add("row");
+  arrOfData.forEach((e) => {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.textContent = e;
+    row.appendChild(cell);
+  });
+  addButtons(row);
+  container.appendChild(row);
+};
+
+const addButtons = (row) => {
+  const editBtn = document.createElement("button");
+  row.appendChild(editBtn);
+  editBtn.innerText = "Edit";
+  editBtn.classList.add("btn", "edit-btn");
+  const deleteBtn = document.createElement("button");
+  row.appendChild(deleteBtn);
+  deleteBtn.innerText = "Delete";
+  deleteBtn.classList.add("btn", "delete-btn");
+};
+
+const drawTable = (arrOfData) => {
+  arrOfData.forEach((person) => {
+    addRow([
+      person.id,
+      person.firstName,
+      person.lastName,
+      person.capsule,
+      person.age,
+      person.city,
+      person.gender,
+      person.hobby,
+    ]);
+  });
+};
+const paintPage = async () => {
+  drawTable(await getArrOfPeople());
+};
+paintPage();
